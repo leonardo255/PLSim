830fa54bc52a4913bc27c97b73f564a77e00cb4a
leonardo255 <80691329+leonardo255@users.noreply.github.com>
2024-02-11 14:18:39 +0100

    Initial Commit

---
+++ PLSim/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PLSim
+//
+//  Created by Lennart on 11/02/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
